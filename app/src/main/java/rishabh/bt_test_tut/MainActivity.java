8f3b2b7bc0a2a29806c7de821ec7c71c5c7e87ba
rksksm <rksksm@gmail.com>
2015-06-30 07:50:47 +0530

    initial commit schema of app is created

---
+++ app/src/main/java/rishabh/bt_test_tut/MainActivity.java
@@ -0,0 +1,73 @@
+package rishabh.bt_test_tut;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final int REQUEST_ENABLE_BT = 15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1 = (Button) findViewById(R.id.button);
+                b1.setOnClickListener(new View.OnClickListener()
+                {
+                    public void onClick(View v)
+                    {
+
+                        Intent i = new Intent(getApplicationContext(), BT_screen.class);
+                        startActivity(i);
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void login(View v)
+    {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        //------------------------------------------------------------------------------------
+
+        
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
